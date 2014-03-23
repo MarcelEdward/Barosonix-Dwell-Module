9ff6a27d72feea0b898d974c093d25ac0f0aba14
H-H-H <ginge264@gmail.com>
2014-03-23 23:35:50 +0000

    Initial Import

---
+++ sql/dwell.sql
@@ -0,0 +1,8 @@
+ CREATE TABLE IF NOT EXISTS `landDwell` 
+(
+ 
+`id` char(36) NOT NULL,
+`pid` char(36) NOT NULL,
+`timestamp` int(20) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
